1869eb22ece98c033eb56668b3fb9db3d15671b3
Raul <raul.bricio@gmail.com>
2024-03-04 12:42:46 +0100

    Ejercicio 6

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,141 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
+	let formularioEnviado = false;
+	let entrar = false;
+	let regexp_password = /^(?=.*[a-zA-Z])(?=.*\d).+$/;
+	let regexp_phone = /^\+?(\d[\s-]?)?(\(\d{1,3}\)[\s-]?)?[\d\s-]{7,15}$/;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	let fName = document.getElementById("fName");
+	let fEmail = document.getElementById("fEmail");
+	let fAddress = document.getElementById("fAddress");
+	let fLastN = document.getElementById("fLastN");
+	let fPassword = document.getElementById("fPassword");
+	let fPhone = document.getElementById("fPhone");
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
+	let errorName = document.getElementById("errorName");
+	let errorEmail = document.getElementById("errorEmail");  
+	let errorAddress = document.getElementById("errorAddress");  
+	let errorLastN = document.getElementById("errorLastN");  
+	let errorPassword = document.getElementById("errorPassword");  
+	let errorPhone = document.getElementById("errorPhone"); 
 	
-	// Validate fields entered by the user: name, phone, password, and email
+	const errorMessages = document.querySelectorAll('.invalid-feedback');
+	errorMessages.forEach(function(message) {
+	  message.style.display = 'none'; // Ocultar el mensaje
+	  // O, si prefieres eliminar el texto:
+	  // message.innerText = '';
+	});
+  
+	// Restablecer estilos de los campos de entrada
+	const inputFields = document.querySelectorAll('.form-control');
+	inputFields.forEach(function(field) {
+	  field.style.border = ""; // Restablecer el borde a su estilo original
+	});
+	
+	document.getElementById("miFormulario").addEventListener("submit", (e) => {
+		e.preventDefault();
+		validate(); // Asegúrate de que esta función esté definida en alguna parte de tu código
+	  });
+	  if (formularioEnviado) {
+        return;
+    }
+
 	if(fName.value == ""){
-		error++;
-	}
+		errorName.innerHTML = "El campo nombre es obligatorio.";
+		fName.style.border = "3px solid red";
+		errorName.style.display = "block"
+		entrar = true;
+	} else if(fName.value.length < 3){
+		fName.style.border = "3px solid red";
+		errorName.style.display = "block";
+		entrar = true;
+	} else if(!fName.value.match(/^[a-zA-Z]+$/)){
+		fName.style.border = "3px solid red";
+		errorName.innerHTML = "El campo nombre solo admite texto.";
+		fName.style.border = "3px solid red";
+		errorName.style.display = "block"
+		entrar = true;;
+	} else (fName.style.border = "3px solid green");
+
 
 	if(fEmail.value == ""){
-		error++;
-	}
+		fEmail.style.border = "3px solid red";
+		errorEmail.style.display = "block"
+		entrar = true;
+	} else if(fEmail.value.length < 3){
+		fEmail.style.border = "3px solid red";
+		errorEmail.style.display = "block"
+		entrar = true;
+	} else if(!fEmail.value.match(/^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/)){
+		fEmail.style.border = "3px solid red";
+		errorEmail.style.display = "block"
+		entrar = true;
+	} (fEmail.style.border = "3px solid green")
+
+	if(fAddress.value == ""){
+		fAddress.style.border = "3px solid red";
+		errorAddress.style.display = "block"
+		entrar = true;
+	} else if(fAddress.value.length < 3){
+		fAddress.style.border = "3px solid red";
+		errorAddress.style.display = "block"
+		entrar = true;;
+	} else {fAddress.style.border = "3px solid green"};
+
+	if(fLastN.value == ""){
+		fLastN.style.border = "3px solid red";
+		errorLastN.style.display = "block"
+		entrar = true;
+	} else if(fLastN.value.length < 3){
+		fLastN.style.border = "3px solid red";
+		errorLastN.style.display = "block";
+		entrar = true;
+	} else if(!fLastN.value.match(/^[a-zA-Z]+$/)){
+		fLastN.style.border = "3px solid red";
+		errorLastN.innerHTML = "El campo apellido solo admite texto.";
+		fName.style.border = "3px solid red";
+		errorLastN.style.display = "block"
+		entrar = true;;
+	} else (fLastN.style.border = "3px solid green");
+
+	if(fPassword.value == ""){
+		fPassword.style.border = "3px solid red";
+		errorPassword.style.display = "block"
+		entrar = true;
+	} else if(fPassword.value.length < 3){
+		fPassword.style.border = "3px solid red";
+		errorPassword.style.display = "block";
+		entrar = true;
+	} else if(!fPassword.value.match(regexp_password)){
+		fPassword.style.border = "3px solid red";
+		errorPassword.style.display = "block"
+		entrar = true;;
+	} else (fPassword.style.border = "3px solid green");
+
+	if(fPhone.value == ""){
+		fPhone.style.border = "3px solid red";
+		errorPhone.style.display = "block"
+		entrar = true;
+	} else if(fPhone.value.length < 3){
+		fPhone.style.border = "3px solid red";
+		errorPhone.style.display = "block";
+		entrar = true;
+	} else if(!fPhone.value.match(regexp_phone)){
+		fPhone.style.border = "3px solid red";
+		errorPhone.style.display = "block"
+		entrar = true;;
+	} else (fPhone.style.border = "3px solid green");
 	 
-	if(error>0){
-		alert("Error");
+	if(entrar === true){
+		
+		alert("Revisa los campos");
+		
 	}else{
-		alert("OK");
+		
+		alert("el formulario se ha enviado correctamente");
+		formularioEnviado = true;
 	}
 
 }
--- js/shop.js
+++ js/shop.js
@@ -1,5 +1,5 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
+let products = [
     {
         id: 1,
         name: 'cooking oil',
@@ -70,34 +70,150 @@ var products = [
 // ** Don't hesitate to seek help from your peers or your mentor if you still struggle with debugging.
 
 // Improved version of cartList. Cart is an array of products (objects), but each one has a quantity field to define its quantity, so these products are not repeated.
-var cart = [];
+let cart = [];
 
-var total = 0;
+let total = 0;
 
 // Exercise 1
 function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
     // 2. Add found product to the cart array
+
+// Busca el producto en el array de productos usando el id 
+const product = products.find(item => item.id === id);
+
+// Si el producto existe
+if (product) {
+    // Verifica si el producto ya está en el carrito
+    const idProduct = cart.some(item => item.id === id);
+
+    // Si el producto no está en el carrito
+    if(!idProduct) {
+        // Agrega el producto al carrito con una cantidad de 1
+        cart = [...cart, {
+            id: id , 
+            name: product.name ,
+            price: product.price, 
+            quantity: 1,
+        }];
+    }
+    // Si el producto ya está en el carrito
+    else {
+        // Incrementa la cantidad del producto en el carrito
+        cart = cart.map(item => {
+            if (item.id === id) {
+                return {...item, quantity: item.quantity + 1};
+            } else {
+                return item;
+            }
+        });
+    }
+
+
+}
+calculateTotal();
+printCart(cart);
+applyPromotionsCart();
+let totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+document.getElementById('count_product').textContent = `${totalQuantity}`;
 }
 
 // Exercise 2
 function cleanCart() {
 
+    if (confirm('¿Estas Seguro que deseas borrar el carrito?')) {
+    // Clean the cart array
+    cart = [];
+    // Clean the shopping cart modal
+    printCart(cart);
+    // Clean the total price
+    total = 0;
+    let totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+    document.getElementById('count_product').textContent = `${totalQuantity}`;
+
+
+}
+
+let totalPriceElement = document.getElementById('total_price'); 
+totalPriceElement.textContent = `${ total.toFixed(2)}`;
 }
 
 // Exercise 3
 function calculateTotal() {
-    // Calculate total price of the cart using the "cartList" array
+    
+    const total = cart.reduce((accumulator, item) => {
+        return accumulator + (item.price * item.quantity);
+    }, 0);
+
+    document.getElementById('total_price').textContent = total.toFixed(2);
+    console.log(total);
+    console.log(cart);
 }
+
+
+
 
 // Exercise 4
 function applyPromotionsCart() {
-    // Apply promotions to each item in the array "cart"
+
+    cart.forEach(item => {
+        // Verificar primero si el descuento ya fue aplicado al ítem
+        if (item.discountApplied) {
+            return; // Este return hace que el ciclo continúe con el próximo ítem, "saltándose" el código restante para este ítem
+        }
+    
+        const product = products.find(product => product.id === item.id);
+        if (product.offer && product.offer.number && product.offer.percent) {
+            if (item.quantity >= product.offer.number) {
+                // Solo entra aquí si el ítem no ha tenido descuento aplicado,
+                // hay una oferta válida, y la cantidad es suficiente
+                
+                // Calcular el descuento
+                const discount = (item.price * product.offer.percent) / 100;
+                // Aplicar el descuento al precio del ítem
+                item.price -= discount;
+                // Marcar el ítem con el descuento aplicado
+                item.discountApplied = true;
+            }
+        }
+    });
+    
+    
+    
+
+    
+    calculateTotal();
+    printCart(cart);
+    let totalQuantity = cart.reduce((total, item) => total + item.quantity, 0);
+    document.getElementById('count_product').textContent = `${totalQuantity}`;
+
 }
 
+
 // Exercise 5
-function printCart() {
+function printCart(cart) {
     // Fill the shopping cart modal manipulating the shopping cart dom
+        // Obtén el elemento del cuerpo de la tabla
+        let tbody = document.getElementById('cart_list');
+
+        // Inicializa una variable para el nuevo contenido HTML
+        let html = '';
+    
+        // Recorre los elementos del carrito
+        cart.forEach(item => {
+            // Crea una nueva fila y celdas
+            html += `
+                <tr>
+                    <th scope="row">${item.name}</th>
+                    <td>$${item.price}</td>
+                    <td>${item.quantity}</td>
+                    <td>$${(item.price * item.quantity).toFixed(2)}</td>
+                </tr>
+            `;
+        });
+    
+        // Reemplaza el contenido actual del cuerpo de la tabla con el nuevo HTML
+        tbody.innerHTML = html;
 }
 
 
@@ -111,3 +227,13 @@ function removeFromCart(id) {
 function open_modal() {
     printCart();
 }
+
+
+
+
+
+
+
+
+
+
